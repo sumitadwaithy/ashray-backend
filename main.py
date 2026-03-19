71c345f1a8b9e39c78f8673ce5754f51394eac64
Sumit Adwaithy <sumitadwaithy@gmail.com>
2026-03-19 14:58:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://ashraygroup.in"
+        "https://www.ashraygroup.in"
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
